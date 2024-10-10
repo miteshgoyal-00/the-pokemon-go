733b7bb1b800b17ff57b700122927724281d9858
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-10 20:28:43 +0530

    trainee level and xp calculations

--- models.md
+++ models.md
@@ -1,32 +1,47 @@
 ## Trainee Model
 
-- name (unique)
-- linkedPlatforms
-  - platforms (array - Google, Facebook)
-  - googleId (string)
-  - facebookId (string)
-- level (number 1-50)
-- xp (number)
-- team (Mystic, Valor, Instinct)
-- pokemons (array with objects)
-  - ref Pokemon
-- buddy
-  - ref Pokemon
-  - level
-- badges (array with objects)
-  - name
-  - dateEarned
-- friends (array with objects)
-  - ref Trainee
-  - level
-- achievements (array with objects)
-  - title (string)
-  - description
-  - dateAchieved (date)
-- coins (number)
-- inventory (array with objects)
-  - item (ref InventoryItem Model)
-  - quantity
-- quests (array with objects)
-  - questId (ref Quest)
-  - status (enum - Not Started, In Progress, Completed)
+```markdown
+> name (unique)
+
+> linkedPlatforms
+  * platforms (array - Google, Facebook)
+  * googleId (string)
+  * facebookId (string)
+
+> level (number 1-50)
+> xp (number) (to reach next level)
+> totalXp (number) (lifetime)
+> team (Mystic, Valor, Instinct)
+> coins (number)
+
+> pokemons (array with objects)
+  * ref Pokemon
+
+> buddy
+  * ref Pokemon
+  * level
+
+> badges (array with objects)
+  * name
+  * dateEarned
+
+> friends (array with objects)
+  * ref Trainee
+  * level
+
+> achievements (array with objects)
+  * title (string)
+  * description
+  * dateAchieved (date)
+
+> inventory (array with objects)
+  * item (ref InventoryItem Model)
+  * quantity
+
+> quests (array with objects)
+  * questId (ref Quest)
+  * status (enum - Not Started, In Progress, Completed)
+
+> this.pre('save')
+> FN - calculateLevel(this.xp)
+```
--- src/models/trainee.model.js
+++ src/models/trainee.model.js
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose";
 
-const linkedPlatforms = {
+const linked_platforms = {
     googleId: {
         type: String,
         required: function () {
@@ -34,16 +34,22 @@ const trainee_schema = new Schema({
         required: true,
         unique: true,
     },
-    linkedPlatforms: linkedPlatforms,
+    linkedPlatforms: linked_platforms,
+    // xp to reach next level
+    xp: {
+        type: Number,
+        default: 0,
+    },
+    // lifetime xp earned
+    totalXp: {
+        type: Number,
+        default: 0,
+    },
     level: {
         type: Number,
         default: 1,
         min: 1,
         max: 50,
-    },
-    xp: {
-        type: Number,
-        default: 0,
     },
     team: {
         type: String,
@@ -137,6 +143,34 @@ const trainee_schema = new Schema({
         },
     ],
 });
+
+trainee_schema.pre("save", function (next) {
+    if (this.isModified("xp")) {
+        this.calculateLevel(this.xp);
+    }
+    next();
+});
+
+trainee_schema.methods.calculateLevel = function (xp) {
+    const checkpoints = [
+        0, 5000, 10000, 15000, 20000, 30000, 40000, 50000, 75000, 100000,
+        125000, 150000, 175000, 200000, 300000, 400000, 500000, 600000, 700000,
+        800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000,
+        1600000, 1700000, 1800000, 1900000, 2000000, 2100000, 2200000, 2300000,
+        2400000, 2500000, 2600000, 2700000, 2800000, 2900000, 3000000, 3100000,
+        3200000, 3300000, 3400000, 3500000, 3600000, 3700000, 3800000, 3900000,
+        4000000,
+    ];
+
+    for (let i = checkpoints.length - 1; i >= checkpoints[i]; i--) {
+        if (xp >= checkpoints[i]) {
+            this.level = i + 1;
+            this.totalXp += xp;
+            this.xp = xp - checkpoints[this.level - 1];
+            break;
+        }
+    }
+};
 
 const trainee_model = model("Trainee", trainee_schema);
 
