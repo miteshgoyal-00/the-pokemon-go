aa2da6bbac7ab3557cc3f299b5c3edb7f034787a
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-12 20:55:56 +0530

    minor issue in index file of dotenv config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import db_connection from "./src/db/config.db.js";
 import "./src/utils/console.util.js";
 
 dotenv.config({
-    src: "./config/.env",
+    path: "./config/.env",
 });
 
 const PORT = process.env.PORT || 4000;
