c551bbffe0322e2490e7cfb6bc6c1dc3aa33adbd
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-12 20:10:43 +0530

    added middlewares in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 export default app;
