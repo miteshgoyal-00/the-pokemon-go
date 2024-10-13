7c826d478597a6e057def3d258bd6d6900f6a4a6
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-13 10:04:12 +0530

    created and registered the trainee route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import traineeRouter from "./routes/trainee.routes.js";
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(
         credentials: true,
     })
 );
+
+app.use("/trainee", traineeRouter);
 
 export default app;
