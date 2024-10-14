cc567b27c1f657f006223d23e78864483bf04895
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-14 16:34:28 +0530

    added candies and evolve stage in pokedex pokemon schema.

--- docs/models.md
+++ docs/models.md
@@ -78,6 +78,8 @@
 > type (array with enum strings)
 > weight
 > height
+> candiesHolded (number)
+> evolutionStage (number)
 > evolutions (ref PokedexPokemon)
 ```
 
--- src/models/pokedex-pokemon.model.js
+++ src/models/pokedex-pokemon.model.js
@@ -30,6 +30,23 @@ const pokedex_pokemon_schema = Schema({
     ],
     weight: { type: Number, required: true },
     height: { type: Number, required: true },
+    candiesHolded: {
+        type: Number,
+        default: 3,
+        get: function () {
+            switch (this.evolutionStage) {
+                case 1:
+                    return 3;
+                case 2:
+                    return 5;
+                case 3:
+                    return 10;
+                default:
+                    return 3;
+            }
+        },
+    },
+    evolutionStage: { type: Number, default: 1 },
     evolutions: [{ type: Schema.Types.ObjectId, ref: "PokedexPokemon" }],
 });
 
