bbf5c9d5bfda901ac15637618f0a356af8d9310e
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-14 14:04:02 +0530

    added del inventory-items data in del route

--- src/routes/del.routes.js
+++ src/routes/del.routes.js
@@ -1,16 +1,24 @@
 import { Router } from "express";
 import trainee_model from "../models/trainee.model.js";
+import inventory_item_model from "../models/inventory-item.model.js";
 
 const router = Router();
 
+const json_response = (req, deleted) => ({
+    success: true,
+    deleted_all: true,
+    from_model: req.path.split("/").pop(),
+    deleted_count: deleted.deletedCount,
+});
+
 router.route("/trainees").get(async (req, res, next) => {
     const deleted = await trainee_model.deleteMany();
-    res.json({
-        success: true,
-        delete_all: true,
-        from_model: req.path.split("/").pop(),
-        deleted_count: deleted.deletedCount,
-    });
+    res.json(json_response(req, deleted));
+});
+
+router.route("/inventory-items").get(async (req, res, next) => {
+    const deleted = await inventory_item_model.deleteMany();
+    res.json(json_response(req, deleted));
 });
 
 export default router;
