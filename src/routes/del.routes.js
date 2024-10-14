95bca3bcb67f285a9d72d14d71774eb8e5f0e168
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-14 13:00:44 +0530

    refactoerd json res in del route

--- src/routes/del.routes.js
+++ src/routes/del.routes.js
@@ -4,11 +4,12 @@ import trainee_model from "../models/trainee.model.js";
 const router = Router();
 
 router.route("/trainees").get(async (req, res, next) => {
-    await trainee_model.deleteMany();
+    const deleted = await trainee_model.deleteMany();
     res.json({
         success: true,
         delete_all: true,
         from_model: req.path.split("/").pop(),
+        deleted_count: deleted.deletedCount,
     });
 });
 
