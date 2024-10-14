dd50dbd4229eb029b159ab3696cf73de84867138
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-14 18:01:32 +0530

    created and registered some routes.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import cors from "cors";
 import delRouter from "./routes/del.routes.js";
 import traineeRouter from "./routes/trainee.routes.js";
 import inventoryItemRouter from "./routes/inventory-item.routes.js";
+import instancePokemonRouter from "./routes/instance-pokemon.routes.js";
+import pokedexPokemonRouter from "./routes/pokedex-pokemon.routes.js";
+import questRouter from "./routes/quest.routes.js";
+import achievementRouter from "./routes/achievement.routes.js";
+import badgeRouter from "./routes/badge.routes.js";
 
 const app = express();
 
@@ -20,6 +25,11 @@ app.use(cookieParser());
 
 app.use("/trainee", traineeRouter);
 app.use("/inventory-item", inventoryItemRouter);
+app.use("/instance-pokemon", instancePokemonRouter);
+app.use("/pokedex-pokemon", pokedexPokemonRouter);
+app.use("/quest", questRouter);
+app.use("/achievement", achievementRouter);
+app.use("/badge", badgeRouter);
 
 // just util to clear all data of specific model if needed
 app.use("/del/all/data", delRouter);
