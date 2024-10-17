e9f7dba32680895e8115f187a6a84054ab643c95
Mitesh Goyal <miteshgoyal.00@gmail.com>
2024-10-17 16:01:53 +0530

    improved the writing logic of custom response middleware

--- src/middlewares/response.middleware.js
+++ src/middlewares/response.middleware.js
@@ -1,8 +1,32 @@
-const response_success = (req, res, next) => {
-    res.success = (status, message, data = {}) => {
+// const response_success = (req, res, next) => {
+//     res.success = (status, message, data = {}) => {
+//         res.status(status).json({
+//             response: status,
+//             success: true,
+//             message: message,
+//             ...data,
+//         });
+//     };
+//     next();
+// };
+
+// const response_failure = (req, res, next) => {
+//     res.failure = (status, message, data = {}) => {
+//         res.status(status).json({
+//             response: status,
+//             success: false,
+//             message: message,
+//             ...data,
+//         });
+//     };
+//     next();
+// };
+
+const createResponse = (success) => (req, res, next) => {
+    res[success ? "success" : "failure"] = (status, message, data = {}) => {
         res.status(status).json({
             response: status,
-            success: true,
+            success: success,
             message: message,
             ...data,
         });
@@ -10,16 +34,7 @@ const response_success = (req, res, next) => {
     next();
 };
 
-const response_failure = (req, res, next) => {
-    res.failure = (status, message, data = {}) => {
-        res.status(status).json({
-            response: status,
-            success: false,
-            message: message,
-            ...data,
-        });
-    };
-    next();
-};
+const response_success = createResponse(true);
+const response_failure = createResponse(false);
 
 export default { response_success, response_failure };
